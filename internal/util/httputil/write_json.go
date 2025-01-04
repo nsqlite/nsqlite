632180555f0057214481a69e1445d0f47b78cbf3
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-04 01:31:04 -0600

    Add WriteJSONBytes function to handle byte slice JSON responses

--- internal/util/httputil/write_json.go
+++ internal/util/httputil/write_json.go
@@ -17,3 +17,15 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 
 	return nil
 }
+
+// WriteJSONBytes writes a byte slice as a JSON response to the given http.ResponseWriter.
+func WriteJSONBytes(w http.ResponseWriter, status int, b []byte) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if _, err := w.Write(b); err != nil {
+		return fmt.Errorf("failed to write JSON response: %w", err)
+	}
+
+	return nil
+}
