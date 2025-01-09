a02a3586e5035d85b8f8943279ed3fed2b6c51fd
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-09 05:04:44 +0000

    Add devcontainer configuration with Dockerfile and bash startup script

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,22 @@
+# Define command aliases
+alias t='task'
+alias tt='task test'
+alias tls='task --list'
+alias ls='ls --color=auto'
+alias ll='ls -alhF'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias c='clear'
+alias h='history'
+alias sqlite='sqlite3'
+
+echo "
+    _   _______ ____    __    _ __
+   / | / / ___// __ \  / /   (_) /____
+  /  |/ /\__ \/ / / / / /   / / __/ _ \\
+ / /|  /___/ / /_/ / / /___/ / /_/  __/
+/_/ |_//____/\___\_\/_____/_/\__/\___/
+---------------------------------------
+------------ Dev Container ------------
+---------------------------------------
+"
