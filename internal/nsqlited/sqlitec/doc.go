a6d275d730a883cdb97a3e699686dcfc576b210f
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-17 23:11:33 +0000

    Move package documentation from doc.go to sqlite3.go for better organization and accessibility

--- internal/nsqlited/sqlitec/doc.go
+++
@@ -1,6 +0,0 @@
-// Package sqlitec provides a lightweight wrapper for the SQLite C library.
-// It allows direct interaction with SQLite's low-level API.
-//
-//   - https://www.sqlite.org/cintro.html
-//   - https://www.sqlite.org/c3ref/intro.html
-package sqlitec
--- internal/nsqlited/sqlitec/sqlite3.go
+++ internal/nsqlited/sqlitec/sqlite3.go
@@ -1,3 +1,8 @@
+// Package sqlitec provides a lightweight wrapper for the SQLite C library.
+// It allows direct interaction with SQLite's low-level API.
+//
+//   - https://www.sqlite.org/cintro.html
+//   - https://www.sqlite.org/c3ref/intro.html
 package sqlitec
 
 /*
