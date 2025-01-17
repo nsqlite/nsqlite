22d97b7586179229524883d40c1042f9e2fc34ce
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-17 23:11:02 +0000

    Refactor comments in sqlite3.c to improve clarity and readability of C function definitions for SQLite3 API access

--- internal/nsqlited/sqlitec/sqlite3.c
+++ internal/nsqlited/sqlitec/sqlite3.c
@@ -1,11 +1,9 @@
 #include "sqlite3-v3.48.0.c"
 #include <stdlib.h>
 
-/**
-/*  Here we define some C functions that are needed to access some of the
-/*  SQLite3 C API functions that are not directly accessible from Go and
-/*  other utilities that are useful for this project.
-**/
+// Here we define some C functions that are needed to access some of the
+// SQLite3 C API functions that are not directly accessible from Go and
+// other utilities that are useful for this project.
 
 // SQLITE_TRANSIENT is not accessible from Go, so we create a wrapper here.
 static int cust_sqlite3_bind_text(sqlite3_stmt *stmt, int n, char *p, int np) {
