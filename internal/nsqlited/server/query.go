b67d55dac494f4eb94a4ea29998e06bc429dd3de
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-17 05:47:17 +0000

    Refactor query handler to use latest changes in db

--- internal/nsqlited/server/query.go
+++ internal/nsqlited/server/query.go
@@ -2,79 +2,39 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/nsqlite/nsqlite/internal/nsqlited/db"
+	"github.com/nsqlite/nsqlite/internal/nsqlited/sqlitec"
 	"github.com/nsqlite/nsqlite/internal/util/httputil"
-	"github.com/orsinium-labs/enum"
 )
 
-type resultType enum.Member[string]
+// ResponseResult represents the structure of a query result.
+type ResponseResult struct {
+	Time  float64 `json:"time"`
+	TxId  string  `json:"txId,omitempty"`
+	Error string  `json:"error,omitempty"`
 
-func (r resultType) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`"%s"`, r.Value)), nil
-}
+	LastInsertID int64 `json:"lastInsertId,omitempty"`
+	RowsAffected int64 `json:"rowsAffected,omitempty"`
 
-var (
-	resultTypeWrite    = resultType{"write"}
-	resultTypeRead     = resultType{"read"}
-	resultTypeBegin    = resultType{"begin"}
-	resultTypeCommit   = resultType{"commit"}
-	resultTypeRollback = resultType{"rollback"}
-	resultTypeOk       = resultType{"ok"}
-	resultTypeError    = resultType{"error"}
-)
-
-// WriteResult represents the structure of a write query result.
-type WriteResult struct {
-	Type         resultType `json:"type"`
-	LastInsertID int64      `json:"lastInsertId"`
-	RowsAffected int64      `json:"rowsAffected"`
-	Time         float64    `json:"time"`
-}
-
-// ReadResult represents the structure of a read query result.
-type ReadResult struct {
-	Type    resultType `json:"type"`
-	Columns []string   `json:"columns"`
-	Types   []string   `json:"types"`
-	Values  [][]any    `json:"values"`
-	Time    float64    `json:"time"`
-}
-
-// TxResult represents the structure of a transaction operation result.
-type TxResult struct {
-	Type resultType `json:"type"`
-	TxId string     `json:"txId"`
-	Time float64    `json:"time"`
-}
-
-// SuccessResult represents a generic success result.
-type SuccessResult struct {
-	Type resultType `json:"type"`
-	Time float64    `json:"time"`
-}
-
-// ErrorResult represents the structure of an error result.
-type ErrorResult struct {
-	Type  resultType `json:"type"`
-	Error string     `json:"error"`
-	Time  float64    `json:"time"`
+	Columns []string `json:"columns,omitempty"`
+	Types   []string `json:"types,omitempty"`
+	Rows    [][]any  `json:"rows,omitempty"`
 }
 
 // Response represents the structure of an outgoing response.
 type Response struct {
-	Results []any   `json:"results"`
-	Time    float64 `json:"time"`
+	Time    float64          `json:"time"`
+	Results []ResponseResult `json:"results"`
 }
 
 // Query represents a single query within a request.
 type Query struct {
-	TxId   string `json:"txId"`
-	Query  string `json:"query"`
-	Params []any  `json:"params"`
+	TxId   string               `json:"txId"`
+	Query  string               `json:"query"`
+	Params []sqlitec.QueryParam `json:"params"`
 }
 
 // queryHandler is the HTTP handler for the /query endpoint that
@@ -93,16 +53,15 @@ func (s *Server) queryHandler(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	allStart := time.Now()
-	results := []any{}
+	results := []ResponseResult{}
 
 	for _, q := range queries {
 		thisStart := time.Now()
 
 		if q.Query == "" {
-			results = append(results, ErrorResult{
-				Type:  resultTypeError,
-				Error: "Empty query",
+			results = append(results, ResponseResult{
 				Time:  time.Since(thisStart).Seconds(),
+				Error: "Empty query",
 			})
 			continue
 		}
@@ -113,79 +72,28 @@ func (s *Server) queryHandler(w http.ResponseWriter, r *http.Request) error {
 			Params: q.Params,
 		})
 		if err != nil {
-			results = append(results, ErrorResult{
-				Type:  resultTypeError,
-				Error: err.Error(),
+			results = append(results, ResponseResult{
 				Time:  time.Since(thisStart).Seconds(),
+				Error: err.Error(),
 			})
 			continue
 		}
 
-		if res.Type == db.QueryTypeRead {
-			if res.ReadResult.Values == nil {
-				results = append(results, ErrorResult{
-					Type:  resultTypeError,
-					Error: "No rows returned",
-					Time:  time.Since(thisStart).Seconds(),
-				})
-				continue
-			}
-
-			results = append(results, ReadResult{
-				Type:    resultTypeRead,
-				Columns: res.ReadResult.Columns,
-				Types:   res.ReadResult.Types,
-				Values:  *res.ReadResult.Values,
-				Time:    time.Since(thisStart).Seconds(),
-			})
-			continue
-		}
-
-		if res.Type == db.QueryTypeWrite {
-			results = append(results, WriteResult{
-				Type:         resultTypeWrite,
-				LastInsertID: res.WriteResult.LastInsertID,
-				RowsAffected: res.WriteResult.RowsAffected,
-				Time:         time.Since(thisStart).Seconds(),
-			})
-			continue
-		}
-
-		if res.Type == db.QueryTypeBegin {
-			results = append(results, TxResult{
-				Type: resultTypeBegin,
-				TxId: res.TxId,
-				Time: time.Since(thisStart).Seconds(),
-			})
-			continue
-		}
-
-		if res.Type == db.QueryTypeCommit {
-			results = append(results, TxResult{
-				Type: resultTypeCommit,
-				TxId: res.TxId,
-				Time: time.Since(thisStart).Seconds(),
-			})
-			continue
-		}
-
-		if res.Type == db.QueryTypeRollback {
-			results = append(results, TxResult{
-				Type: resultTypeRollback,
-				TxId: res.TxId,
-				Time: time.Since(thisStart).Seconds(),
-			})
-			continue
-		}
-
-		results = append(results, SuccessResult{
-			Type: resultTypeOk,
+		results = append(results, ResponseResult{
 			Time: time.Since(thisStart).Seconds(),
+			TxId: res.TxId,
+
+			LastInsertID: res.LastInsertID,
+			RowsAffected: res.RowsAffected,
+
+			Columns: res.Columns,
+			Types:   res.Types,
+			Rows:    res.Rows,
 		})
 	}
 
 	return httputil.WriteJSON(w, http.StatusOK, Response{
-		Results: results,
 		Time:    time.Since(allStart).Seconds(),
+		Results: results,
 	})
 }
