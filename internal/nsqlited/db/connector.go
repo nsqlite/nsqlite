ed4fee1798c5f98514619cc667e881de057e232c
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-17 05:46:32 +0000

    Refactor connector initialization to use sqlitedrv.NewConnector for improved readability and maintainability

--- internal/nsqlited/db/connector.go
+++ internal/nsqlited/db/connector.go
@@ -1,28 +1,12 @@
 package db
 
 import (
-	"context"
 	"database/sql/driver"
 
-	"github.com/mattn/go-sqlite3"
+	"github.com/nsqlite/nsqlite/internal/nsqlited/sqlitedrv"
 )
 
-type connector struct {
-	driver     driver.Driver
-	dbPath     string
-	isReadOnly bool
-}
-
-func newConnector(dbPath string, readOnly bool) (driver.Connector, error) {
-	return &connector{
-		driver:     &sqlite3.SQLiteDriver{},
-		dbPath:     dbPath,
-		isReadOnly: readOnly,
-	}, nil
-}
-
-// Connect creates a new database connection with the NSQLite optimizations.
-func (c *connector) Connect(context.Context) (driver.Conn, error) {
+func newConnector(dbPath string, readOnly bool) driver.Connector {
 	optimizations := []string{
 		"PRAGMA JOURNAL_MODE = WAL;",
 		"PRAGMA BUSY_TIMEOUT = 5000;",
@@ -33,36 +17,12 @@ func (c *connector) Connect(context.Context) (driver.Conn, error) {
 		"PRAGMA MMAP_SIZE = 536870912;", // 512MB
 	}
 
-	if c.isReadOnly {
+	if readOnly {
 		optimizations = append(optimizations, "PRAGMA QUERY_ONLY = true;")
 	}
 
-	conn, err := c.driver.Open("file:" + c.dbPath)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, optimization := range optimizations {
-		if err := exec(conn, optimization); err != nil {
-			conn.Close()
-			return nil, err
-		}
-	}
-
-	return conn, nil
-}
-
-func (c *connector) Driver() driver.Driver {
-	return c.driver
-}
-
-func exec(conn driver.Conn, query string) error {
-	stmt, err := conn.Prepare(query)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(nil)
-	return err
+	return sqlitedrv.NewConnector(
+		dbPath,
+		sqlitedrv.WithPostConnectQueries(optimizations),
+	)
 }
