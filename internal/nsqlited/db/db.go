6c8fa3b39b76999451a989c4a38600a34e92d2f1
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2025-01-17 05:46:53 +0000

    Refactor NSQLite db to use new low level C integration

--- internal/nsqlited/db/db.go
+++ internal/nsqlited/db/db.go
@@ -13,15 +13,19 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/mattn/go-sqlite3"
 	"github.com/nsqlite/nsqlite/internal/nsqlited/log"
+	"github.com/nsqlite/nsqlite/internal/nsqlited/sqlitec"
+	"github.com/nsqlite/nsqlite/internal/nsqlited/sqlitedrv"
 	"github.com/nsqlite/nsqlite/internal/nsqlited/stats"
+	"github.com/nsqlite/nsqlite/internal/util/syncutil"
 	"github.com/orsinium-labs/enum"
 )
 
 var (
 	ErrTxNotFound = errors.New("transaction not found or timed out, check your settings")
 	ErrTxWithinTx = errors.New("cannot start a transaction within a transaction")
+	ErrTxOnlyOne  = errors.New("only only one transaction is allowed at a time")
+	ErrTxNotMatch = errors.New("transaction ID does not match the currently active transaction")
 )
 
 // Config represents the configuration for a DB instance.
@@ -40,47 +44,34 @@ type Config struct {
 // DB represents the SQLite integration for NSQLite.
 type DB struct {
 	Config
-	isInitialized           bool
-	readWriteConn           *sql.DB
-	readOnlyConn            *sql.DB
-	transactions            sync.Map // txId -> *transactionData
-	transactionsMonitorStop chan any
-	writeMu                 sync.Mutex
-	closeWg                 sync.WaitGroup
-}
-
-// transactionData holds a *sql.Tx and the last time it was accessed.
-type transactionData struct {
-	tx       *sql.Tx
-	lastUsed time.Time
+	isInitialized     bool
+	readWriteConn     *sql.DB
+	readOnlyConn      *sql.DB
+	txId              syncutil.AtomicString
+	txIdLastUsed      syncutil.AtomicTime
+	txIdleMonitorStop chan any
+	writeMu           sync.Mutex
+	closeWg           sync.WaitGroup
 }
 
 // Query represents a query to be executed.
 type Query struct {
 	TxId   string
 	Query  string
-	Params []any
-}
-
-// WriteResult represents the result of a write query.
-type WriteResult struct {
-	LastInsertID int64
-	RowsAffected int64
-}
-
-// ReadResult represents the result of a read query.
-type ReadResult struct {
-	Columns []string
-	Types   []string
-	Values  *[][]any
+	Params []sqlitec.QueryParam
 }
 
 // QueryResult represents the result of a query.
 type QueryResult struct {
-	Type        queryType
-	TxId        string
-	WriteResult WriteResult
-	ReadResult  ReadResult
+	Type queryType
+	TxId string
+
+	LastInsertID int64
+	RowsAffected int64
+
+	Columns []string
+	Types   []string
+	Rows    [][]any
 }
 
 // NewDB creates a new DB instance.
@@ -99,16 +90,8 @@ func NewDB(config Config) (*DB, error) {
 	}
 
 	databasePath := path.Join(config.DataDirectory, "database.sqlite")
-
-	readWriteConnector, err := newConnector(databasePath, false)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create read-write connector: %w", err)
-	}
-
-	readOnlyConnector, err := newConnector(databasePath, true)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create read-only connector: %w", err)
-	}
+	readWriteConnector := newConnector(databasePath, false)
+	readOnlyConnector := newConnector(databasePath, true)
 
 	readWriteConn := sql.OpenDB(readWriteConnector)
 	if err := readWriteConn.Ping(); err != nil {
@@ -125,24 +108,62 @@ func NewDB(config Config) (*DB, error) {
 	}
 	readOnlyConn.SetConnMaxIdleTime(0)
 	readOnlyConn.SetConnMaxLifetime(0)
-	readOnlyConn.SetMaxIdleConns(5)
+	readOnlyConn.SetMaxIdleConns(100)
 
 	db := &DB{
-		Config:                  config,
-		isInitialized:           true,
-		readWriteConn:           readWriteConn,
-		readOnlyConn:            readOnlyConn,
-		transactions:            sync.Map{},
-		transactionsMonitorStop: make(chan any),
-		writeMu:                 sync.Mutex{},
-		closeWg:                 sync.WaitGroup{},
+		Config:            config,
+		isInitialized:     true,
+		readWriteConn:     readWriteConn,
+		readOnlyConn:      readOnlyConn,
+		txId:              *syncutil.NewAtomicString(""),
+		txIdLastUsed:      *syncutil.NewAtomicTime(time.Now()),
+		txIdleMonitorStop: make(chan any),
+		writeMu:           sync.Mutex{},
+		closeWg:           sync.WaitGroup{},
 	}
 
 	db.closeWg.Add(1)
-	go db.monitorIdleTransactions(config.TxIdleTimeout)
+	go db.txIdleMonitor(config.TxIdleTimeout)
 
 	config.Logger.InfoNs(log.NsDatabase, "database started")
 	return db, nil
+}
+
+// getRawConn returns a raw connection from *sql.DB and a function to return
+// it to the pool.
+func (db *DB) getRawConn(ctx context.Context, dbPool *sql.DB) (*sqlitec.Conn, func() error, error) {
+	dConn, err := dbPool.Conn(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get connection from pool: %w", err)
+	}
+
+	var sqlitecConn *sqlitec.Conn
+	err = dConn.Raw(func(driverConn any) error {
+		dc, ok := driverConn.(*sqlitedrv.Conn)
+		if !ok {
+			return fmt.Errorf("failed to cast driver connection")
+		}
+		sqlitecConn = dc.RawConn()
+		return nil
+	})
+	if err != nil {
+		dConn.Close()
+		return nil, nil, fmt.Errorf("failed to get raw connection: %w", err)
+	}
+
+	return sqlitecConn, dConn.Close, nil
+}
+
+// getReadWriteRawConn returns the read-write connection and a function to
+// return it to the pool.
+func (db *DB) getReadWriteRawConn(ctx context.Context) (*sqlitec.Conn, func() error, error) {
+	return db.getRawConn(ctx, db.readWriteConn)
+}
+
+// getReadOnlyRawConn returns the read-only connection and a function to return it
+// to the pool.
+func (db *DB) getReadOnlyRawConn(ctx context.Context) (*sqlitec.Conn, func() error, error) {
+	return db.getRawConn(ctx, db.readOnlyConn)
 }
 
 // IsInitialized returns whether the DB instance is initialized.
@@ -150,54 +171,35 @@ func (db *DB) IsInitialized() bool {
 	return db.isInitialized
 }
 
-// monitorIdleTransactions rolls back transactions not used within the timeout.
-func (db *DB) monitorIdleTransactions(timeout time.Duration) {
+// txIdleMonitor rolls back the current transaction if not used within the timeout.
+func (db *DB) txIdleMonitor(timeout time.Duration) {
 	defer db.closeWg.Done()
 	ticker := time.NewTicker(timeout)
 	defer ticker.Stop()
 
 	for {
 		select {
-		case <-db.transactionsMonitorStop:
+		case <-db.txIdleMonitorStop:
 			return
 		case <-ticker.C:
-			now := time.Now()
-			db.transactions.Range(func(key, value any) bool {
-				txId, ok1 := key.(string)
-				txData, ok2 := value.(*transactionData)
-				if !ok1 || !ok2 {
-					return true
-				}
-
-				if now.Sub(txData.lastUsed) > timeout {
-					_ = txData.tx.Rollback()
-					db.transactions.Delete(txId)
-				}
-
-				return true
-			})
+			if db.txId.Load() == "" {
+				continue
+			}
+			if time.Now().Sub(db.txIdLastUsed.Load()) > timeout {
+				_, _ = db.executeRollbackQuery(context.Background(), db.txId.Load())
+			}
 		}
 	}
 }
 
 // Close attempts a graceful shutdown of everything this DB manages.
 func (db *DB) Close() error {
-	close(db.transactionsMonitorStop)
-
+	close(db.txIdleMonitorStop)
 	db.closeWg.Wait()
 
-	db.transactions.Range(func(key, value any) bool {
-		txID, ok1 := key.(string)
-		txData, ok2 := value.(*transactionData)
-		if !ok1 || !ok2 {
-			return true
-		}
-
-		_ = txData.tx.Rollback()
-		db.transactions.Delete(txID)
-
-		return true
-	})
+	if db.txId.Load() != "" {
+		_, _ = db.executeRollbackQuery(context.Background(), db.txId.Load())
+	}
 
 	if db.readWriteConn != nil {
 		if err := db.readWriteConn.Close(); err != nil {
@@ -240,29 +242,19 @@ func (db *DB) detectQueryType(ctx context.Context, query string) (queryType, err
 		return QueryTypeRollback, nil
 	}
 
-	conn, err := db.readOnlyConn.Conn(ctx)
+	conn, returnConn, err := db.getReadOnlyRawConn(ctx)
 	if err != nil {
 		return QueryTypeUnknown, fmt.Errorf("failed to get connection: %w", err)
 	}
-	defer conn.Close()
+	defer func() { _ = returnConn() }()
 
-	isReadOnly := false
-	err = conn.Raw(func(driverConn any) error {
-		sqliteConn := driverConn.(*sqlite3.SQLiteConn)
-		drvStmt, err := sqliteConn.Prepare(query)
-		if err != nil {
-			return err
-		}
-		defer drvStmt.Close()
-		sqliteStmt := drvStmt.(*sqlite3.SQLiteStmt)
-		isReadOnly = sqliteStmt.Readonly()
-		return nil
-	})
+	stmt, err := conn.Prepare(query)
 	if err != nil {
 		return QueryTypeUnknown, fmt.Errorf("failed to prepare statement: %w", err)
 	}
+	defer func() { _ = stmt.Finalize() }()
 
-	if isReadOnly {
+	if stmt.ReadOnly() {
 		return QueryTypeRead, nil
 	}
 	return QueryTypeWrite, nil
@@ -285,17 +277,13 @@ func (db *DB) query(ctx context.Context, query Query) (QueryResult, error) {
 		return QueryResult{}, fmt.Errorf("failed to detect query type: %w", err)
 	}
 
-	if typeOfQuery == QueryTypeBegin && query.TxId != "" {
-		return QueryResult{}, ErrTxWithinTx
-	}
-
 	switch typeOfQuery {
 	case QueryTypeBegin:
-		return db.executeBeginQuery()
+		return db.executeBeginQuery(ctx, query.TxId)
 	case QueryTypeCommit:
-		return db.executeCommitQuery(query)
+		return db.executeCommitQuery(ctx, query.TxId)
 	case QueryTypeRollback:
-		return db.executeRollbackQuery(query)
+		return db.executeRollbackQuery(ctx, query.TxId)
 	case QueryTypeRead:
 		return db.executeReadQuery(ctx, query)
 	case QueryTypeWrite:
@@ -305,94 +293,110 @@ func (db *DB) query(ctx context.Context, query Query) (QueryResult, error) {
 	return QueryResult{}, fmt.Errorf("unknown query type: %s", typeOfQuery.Value)
 }
 
-// executeBeginQuery executes a begin query.
-func (db *DB) executeBeginQuery() (QueryResult, error) {
-	tx, err := db.readWriteConn.Begin()
+// executeBeginQuery executes a begin query using the read-write connection.
+func (db *DB) executeBeginQuery(ctx context.Context, queryTxId string) (QueryResult, error) {
+	// TODO: Add support for queuing transactions when one is already active.
+	if db.txId.Load() != "" {
+		return QueryResult{}, ErrTxWithinTx
+	}
+
+	if db.isCurrentTx(queryTxId) {
+		return QueryResult{}, ErrTxWithinTx
+	}
+
+	conn, returnConn, err := db.getReadWriteRawConn(ctx)
 	if err != nil {
+		return QueryResult{}, fmt.Errorf("failed to get read-write connection from pool: %w", err)
+	}
+	defer func() { _ = returnConn() }()
+
+	if _, err = conn.Query("BEGIN TRANSACTION", nil); err != nil {
 		return QueryResult{}, fmt.Errorf("failed to begin transaction: %w", err)
 	}
 
-	txId, err := uuid.NewRandom()
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to generate transaction ID: %w", err)
-	}
-
-	db.transactions.Store(txId.String(), &transactionData{
-		tx:       tx,
-		lastUsed: time.Now(),
-	})
-
+	txId := uuid.NewString()
+	db.txId.Store(txId)
+	db.txIdLastUsed.Store(time.Now())
 	db.DBStats.IncBegins()
+
 	return QueryResult{
 		Type: QueryTypeBegin,
-		TxId: txId.String(),
+		TxId: txId,
 	}, nil
 }
 
-// executeCommitQuery commits an existing transaction.
-func (db *DB) executeCommitQuery(query Query) (QueryResult, error) {
-	tx, found, _ := db.getTransactionById(query.TxId)
-	if !found {
+// executeCommitQuery commits the existing transaction with the given ID.
+func (db *DB) executeCommitQuery(ctx context.Context, queryTxId string) (QueryResult, error) {
+	if !db.isCurrentTx(queryTxId) {
 		return QueryResult{}, ErrTxNotFound
 	}
-	if err := tx.Commit(); err != nil {
+
+	conn, returnConn, err := db.getReadWriteRawConn(ctx)
+	if err != nil {
+		return QueryResult{}, fmt.Errorf("failed to get read-write connection from pool: %w", err)
+	}
+	defer func() { _ = returnConn() }()
+
+	if _, err = conn.Query("COMMIT", nil); err != nil {
 		return QueryResult{}, fmt.Errorf("failed to commit transaction: %w", err)
 	}
 
-	db.transactions.Delete(query.TxId)
+	db.txId.Store("")
+	db.txIdLastUsed.Store(time.Now())
 	db.DBStats.IncCommits()
 
 	return QueryResult{
 		Type: QueryTypeCommit,
-		TxId: query.TxId,
+		TxId: queryTxId,
 	}, nil
 }
 
 // executeRollbackQuery rolls back an existing transaction.
-func (db *DB) executeRollbackQuery(query Query) (QueryResult, error) {
-	tx, found, _ := db.getTransactionById(query.TxId)
-	if !found {
+func (db *DB) executeRollbackQuery(ctx context.Context, queryTxId string) (QueryResult, error) {
+	if !db.isCurrentTx(queryTxId) {
 		return QueryResult{}, ErrTxNotFound
 	}
 
-	if err := tx.Rollback(); err != nil {
+	conn, returnConn, err := db.getReadWriteRawConn(ctx)
+	if err != nil {
+		return QueryResult{}, fmt.Errorf("failed to get read-write connection from pool: %w", err)
+	}
+	defer func() { _ = returnConn() }()
+
+	if _, err = conn.Query("ROLLBACK", nil); err != nil {
 		return QueryResult{}, fmt.Errorf("failed to rollback transaction: %w", err)
 	}
 
-	db.transactions.Delete(query.TxId)
+	db.txId.Store("")
+	db.txIdLastUsed.Store(time.Now())
 	db.DBStats.IncRollbacks()
 
 	return QueryResult{
 		Type: QueryTypeRollback,
-		TxId: query.TxId,
+		TxId: queryTxId,
 	}, nil
 }
 
-// getTransactionById returns a transaction by its ID and updates its
-// lastUsed time.
-//
-//   - If txId is empty, it returns false without an error.
-//   - If txId is not empty, it returns false with an error if the transaction
-//     is not found.
-//   - If txId is not empty and the transaction is found, it returns true
-//     without an error.
-func (db *DB) getTransactionById(txId string) (*sql.Tx, bool, error) {
+// isCurrentTx returns true if the provided transaction ID is the current one.
+// it also updates the lastUsed time.
+func (db *DB) isCurrentTx(txId string) bool {
+	current := db.txId.Load()
+	if txId == "" || current == "" || txId != current {
+		return false
+	}
+
+	db.txIdLastUsed.Store(time.Now())
+	return true
+}
+
+// matchCurrentTx returns true if the provided transaction ID is empty or matches
+// the current transaction ID.
+func (db *DB) matchCurrentTx(txId string) bool {
 	if txId == "" {
-		return nil, false, nil
+		return true
 	}
 
-	value, found := db.transactions.Load(txId)
-	if !found {
-		return nil, false, ErrTxNotFound
-	}
-
-	txData, ok := value.(*transactionData)
-	if !ok {
-		return nil, false, ErrTxNotFound
-	}
-
-	txData.lastUsed = time.Now()
-	return txData.tx, true, nil
+	return db.isCurrentTx(txId)
 }
 
 // executeWriteQuery increments the write queue count, sends the task,
@@ -404,146 +408,58 @@ func (db *DB) executeWriteQuery(ctx context.Context, query Query) (QueryResult, 
 	db.writeMu.Lock()
 	defer db.writeMu.Unlock()
 
-	tx, found, err := db.getTransactionById(query.TxId)
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to get transaction: %w", err)
+	if !db.matchCurrentTx(query.TxId) {
+		return QueryResult{}, ErrTxNotMatch
 	}
 
-	var result sql.Result
-	if found {
-		result, err = tx.ExecContext(ctx, query.Query, query.Params...)
-	} else {
-		result, err = db.readWriteConn.ExecContext(ctx, query.Query, query.Params...)
+	conn, returnConn, err := db.getReadWriteRawConn(ctx)
+	if err != nil {
+		return QueryResult{}, fmt.Errorf("failed to get read-write connection from pool: %w", err)
 	}
+	defer func() { _ = returnConn() }()
+
+	res, err := conn.Query(query.Query, query.Params)
 	if err != nil {
 		return QueryResult{}, fmt.Errorf("failed to execute write query: %w", err)
 	}
 
-	lastInsertId, err := result.LastInsertId()
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to get last insert ID: %w", err)
-	}
-
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to get rows affected: %w", err)
-	}
-
 	db.DBStats.IncWrites()
 	return QueryResult{
-		TxId: query.TxId,
-		Type: QueryTypeWrite,
-		WriteResult: WriteResult{
-			LastInsertID: lastInsertId,
-			RowsAffected: rowsAffected,
-		},
+		TxId:         query.TxId,
+		Type:         QueryTypeWrite,
+		LastInsertID: res.LastInsertID,
+		RowsAffected: res.RowsAffected,
+		Columns:      res.Columns,
+		Types:        res.Types,
+		Rows:         res.Rows,
 	}, nil
 }
 
 // executeReadQuery executes a read query.
 func (db *DB) executeReadQuery(ctx context.Context, query Query) (QueryResult, error) {
-	tx, found, err := db.getTransactionById(query.TxId)
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to get transaction: %w", err)
+	if !db.matchCurrentTx(query.TxId) {
+		return QueryResult{}, ErrTxNotMatch
 	}
 
-	var result *sql.Rows
-	if found {
-		result, err = tx.QueryContext(ctx, query.Query, query.Params...)
-	} else {
-		result, err = db.readOnlyConn.QueryContext(ctx, query.Query, query.Params...)
+	conn, returnConn, err := db.getReadOnlyRawConn(ctx)
+	if err != nil {
+		return QueryResult{}, fmt.Errorf("failed to get connection: %w", err)
 	}
+	defer func() { _ = returnConn() }()
+
+	res, err := conn.Query(query.Query, query.Params)
 	if err != nil {
 		return QueryResult{}, fmt.Errorf("failed to execute read query: %w", err)
-	}
-	defer result.Close()
-
-	columns, err := result.Columns()
-	if err != nil {
-		return QueryResult{}, fmt.Errorf("failed to get columns: %w", err)
-	}
-
-	types, typesOk := []string{}, false
-	values := [][]any{}
-	for result.Next() {
-		row := make([]any, len(columns))
-		rowPtrs := make([]any, len(columns))
-
-		for i := range rowPtrs {
-			rowPtrs[i] = &row[i]
-		}
-
-		if err = result.Scan(rowPtrs...); err != nil {
-			return QueryResult{}, fmt.Errorf("failed to scan row: %w", err)
-		}
-
-		if !typesOk {
-			enhancedTypes, err := db.getColumnTypes(result, row)
-			if err != nil {
-				return QueryResult{}, fmt.Errorf("failed to get column types: %w", err)
-			}
-			types, typesOk = enhancedTypes, true
-		}
-
-		values = append(values, row)
 	}
 
 	db.DBStats.IncReads()
 	return QueryResult{
-		TxId: query.TxId,
-		Type: QueryTypeRead,
-		ReadResult: ReadResult{
-			Columns: columns,
-			Types:   types,
-			Values:  &values,
-		},
+		TxId:         query.TxId,
+		Type:         QueryTypeRead,
+		LastInsertID: res.LastInsertID,
+		RowsAffected: res.RowsAffected,
+		Columns:      res.Columns,
+		Types:        res.Types,
+		Rows:         res.Rows,
 	}, nil
-}
-
-// getColumnTypes returns the column types for a read query.
-//
-// It tries to get the column types from the result, but if it has empty
-// types, it tries inferring them from the first row following the SQLite
-// datatypes documentation https://www.sqlite.org/datatype3.html.
-func (db *DB) getColumnTypes(result *sql.Rows, singleRow []any) ([]string, error) {
-	types, err := result.ColumnTypes()
-	if err != nil {
-		return []string{}, fmt.Errorf("failed to get column types: %w", err)
-	}
-
-	typeNames := make([]string, len(types))
-	hasEmptyTypes := false
-	for i, t := range types {
-		typeNames[i] = strings.ToLower(t.DatabaseTypeName())
-		if typeNames[i] == "" {
-			hasEmptyTypes = true
-		}
-	}
-
-	if !hasEmptyTypes {
-		return typeNames, nil
-	}
-
-	for i := range typeNames {
-		if typeNames[i] != "" {
-			continue
-		}
-
-		switch singleRow[i].(type) {
-		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-			typeNames[i] = "integer"
-		case float32, float64:
-			typeNames[i] = "real"
-		case bool:
-			typeNames[i] = "boolean"
-		case []byte:
-			typeNames[i] = "blob"
-		case string:
-			typeNames[i] = "text"
-		default:
-			typeNames[i] = ""
-		}
-	}
-
-	return typeNames, nil
 }
